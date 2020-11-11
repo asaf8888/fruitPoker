636ac405f34edb6dda72f3f2f2fdc7b1b66471ee
asaf8888 <asaf2048@gmail.com>
2020-11-11 15:40:49 +0200

    added the runGame func which is the basic game without buttons and card evaluation

--- src/Game.java
+++ src/Game.java
@@ -24,13 +24,10 @@ public class Game {
         }
     }
 
-    public void runGame(int moneyStart){
-        int activePlayer = 0;
-        this.allDraw(3);
-        int highestBid = moneyStart/20;
+    public void runRound(int activePlayer, int highestBid, ArrayList<Player> folded){
         while(true) {
             for (int i = 0; i < this.players.size(); i++) {
-
+                highestBid = this.choose(activePlayer+i, highestBid, folded);
             }
             s:
             {
@@ -39,9 +36,56 @@ public class Game {
                         break s;
                     }
                 }
-            break;
+                break;
             }
         }
+    }
+
+    public boolean foldCheck(ArrayList<Player> folded){
+        if (folded.size() == this.players.size()-1){
+            for (int i = 0; i < this.players.size(); i++) {
+                if(!folded.contains(this.players.get(i))){
+                    this.players.get(i).transfer(-this.moneyPile);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void runGame(int highestBid){
+        int activePlayer = 0;
+        this.allDraw(3);
+        this.players.get(activePlayer).setMoneyBuffer(highestBid/2);
+        activePlayer++;
+        this.players.get(activePlayer).setMoneyBuffer(highestBid);
+        activePlayer++;
+        ArrayList<Player> folded =new ArrayList<>();
+        this.runRound(activePlayer, highestBid, folded);
+        if(this.foldCheck(folded)){
+            return;
+        }
+        this.middlePile.open();
+        for (int i = 0; i < this.players.size(); i++) {
+            this.players.get((i+activePlayer)%this.players.size()).discard(this.chooseDiscard((i+activePlayer)%this.players.size()));
+        }
+        this.runRound(activePlayer, 0, folded);
+        if(this.foldCheck(folded)){
+            return;
+        }
+        this.middlePile.open();
+        this.runRound(activePlayer, 0, folded);
+        if(this.foldCheck(folded)){
+            return;
+        }
+        this.middlePile.open();
+        this.runRound(activePlayer, 0, folded);
+        if(this.foldCheck(folded)){
+            return;
+        }
+        this.finishGame(folded);
+
+
     }
 
     public void call(int highestBid, int playerIndex){
@@ -52,7 +96,25 @@ public class Game {
         this.players.get(playerIndex).transfer(desiredAmount - this.players.get(playerIndex).getMoneyBuffer());
     }
 
-    public void fold(int playerIndex){
+    public void fold(int playerIndex, ArrayList<Player> folded){
         this.players.get(playerIndex).setMoneyBuffer(0);
+        folded.add(this.players.get(playerIndex));
+    }
+
+    public int choose(int playerIndex, int highestBid, ArrayList<Player> folded){
+        if (!folded.contains(this.players.get(playerIndex))) {
+
+
+            //choice of action requires gui
+        }
+        return 1;
+    }
+    public int chooseDiscard(int playerIndex){
+        //choice of discard requires gui
+        return 1;
+    }
+
+    public void finishGame(ArrayList<Player> folded){
+
     }
 }
